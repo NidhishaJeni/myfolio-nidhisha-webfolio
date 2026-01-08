6abf7e4663d2b8482f0b7878571d1c95bfa15107
Nidhisha Jeni <nidhishajeni.27it@licet.ac.in>
2026-01-08 22:07:30 +0530

    Fix profile image path for Vercel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,7 @@
 function Home() {
   return (
-    <div id="home" className="section" style={{ textAlign: "center" }}>
-      
-      {/* PROFILE IMAGE */}
-      <img 
-        src="/profile.jpg" 
-        alt="Profile" 
-        className="profile-img"
-      />
-
+    <div id="home" className="section">
+      <img src="/profile.JPG" alt="Profile" className="profile-img" />
       <h1>A.R. NIDHISHA JENI</h1>
       <p>Student</p>
     </div>
